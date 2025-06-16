678e20e11a225e9652df1a3d99a6d3ce988be26f
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-16 11:59:42 +0530

    [Prashant Chaudhary] Add. PosNegEvenOddCompare program

--- Arrays/PosNegEvenOddCompare.java
+++ Arrays/PosNegEvenOddCompare.java
@@ -4,7 +4,6 @@ public class PosNegEvenOddCompare {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] numbers = new int[5];
-
         for (int i = 0; i < 5; i++) {
             System.out.print("Enter number " + (i + 1) + ": ");
             numbers[i] = sc.nextInt();
