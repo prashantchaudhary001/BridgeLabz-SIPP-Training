78bd46cbb8057fa7169593704a4140be3d28c18b
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-17 11:43:59 +0530

    [Prashant Chaudhary] Add. Calculate WindChill Method program

--- Methods/WindChill.java
+++ Methods/WindChill.java
@@ -4,7 +4,6 @@ public class WindChill {
     public static double calculateWindChill(double temperature, double windSpeed) {
         return 35.74 + 0.6215 * temperature + (0.4275 * temperature - 35.75) * windSpeed;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double temp = scanner.nextDouble();
