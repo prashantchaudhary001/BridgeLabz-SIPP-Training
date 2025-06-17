6b51e3eb2dec232653b85e60cc9fe2383304446b
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-17 11:14:11 +0530

    [Prashant Chaudhary] Add. Handshakes Method program

--- Methods/Handshakes.java
+++ Methods/Handshakes.java
@@ -4,7 +4,6 @@ public class Handshakes {
     public static int calculateHandshakes(int numberOfStudents) {
         return (numberOfStudents * (numberOfStudents - 1)) / 2;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numberOfStudents = scanner.nextInt();
