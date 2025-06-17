b137a16a4891c460392f5b8a9861013386d5af15
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-17 11:38:08 +0530

    [Prashant Chaudhary] Add. Sum Of Natural Number Recursively Method program

--- Methods/SumNaturalNumbersRecursive.java
+++ Methods/SumNaturalNumbersRecursive.java
@@ -9,7 +9,6 @@ public class SumNaturalNumbersRecursive {
     public static int sumFormula(int n) {
         return (n * (n + 1)) / 2;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
