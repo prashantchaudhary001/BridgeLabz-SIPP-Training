7cb35e03b43165610c7bb4ddfb3cab2e0fb1a042
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-17 11:37:00 +0530

    [Prashant Chaudhary] Add. Natural Number sum Method program

--- Methods/SumNaturalNumbers.java
+++ Methods/SumNaturalNumbers.java
@@ -8,7 +8,6 @@ public class SumNaturalNumbers {
         }
         return sum;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
