df679539f659cdf383a8bf6a9a70859cd1ed8c9f
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-17 11:31:08 +0530

    [Prashant Chaudhary] Add. find QuotientReaminder Method program

--- Methods/QuotientRemainder.java
+++ Methods/QuotientRemainder.java
@@ -7,7 +7,6 @@ public class QuotientRemainder {
         result[1] = number % divisor;
         return result;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
