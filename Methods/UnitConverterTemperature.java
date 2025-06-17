9ecd5382e12be9e3b660a09f5f9881db4efd1a4d
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-17 11:42:36 +0530

    [Prashant Chaudhary] Add. UnitConverter of Temperature Method program

--- Methods/UnitConverterTemperature.java
+++ Methods/UnitConverterTemperature.java
@@ -8,7 +8,6 @@ public class UnitConverterTemperature {
     public static double convertCelsiusToFahrenheit(double celsius) {
         return (celsius * 9 / 5) + 32;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double fahrenheit = scanner.nextDouble();
