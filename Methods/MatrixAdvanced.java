0970ce00f6b55452106dc9f88be67b29605ec295
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-17 11:16:32 +0530

    [Prashant Chaudhary] Add. AdvancedMatrix Method program

--- Methods/MatrixAdvanced.java
+++ Methods/MatrixAdvanced.java
@@ -10,7 +10,6 @@ public class MatrixAdvanced {
         }
         return matrix;
     }
-
     public static int[][] transposeMatrix(int[][] matrix) {
         int[][] result = new int[matrix[0].length][matrix.length];
         for (int i = 0; i < matrix.length; i++) {
