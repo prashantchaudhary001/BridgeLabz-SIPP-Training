4901bd061c350f269c724f436db14c0c24e7650d
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-18 12:06:45 +0530

    [Prashant Chaudhary] Add. a program to return all the characters in a string using the user-defined method, compare the result with the String built-in toCharArray() method, and display the result

--- String/AllCharacters.java
+++ String/AllCharacters.java
@@ -15,7 +15,6 @@ public class AllCharacters {
         }
         return true;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
