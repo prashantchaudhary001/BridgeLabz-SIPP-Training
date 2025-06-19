43c2d20989ffb88438e0f9edee7f713ec5082dff
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-19 20:27:37 +0530

    [Prashant Chaudhary] Add. a program to convert Celsius to fahrenheit

--- Day1_ProgrammingElements/level-1/CelsiusToFahrenheit.java
+++ Day1_ProgrammingElements/level-1/CelsiusToFahrenheit.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class CelsiusToFahrenheit {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter temperature in Celsius: ");
