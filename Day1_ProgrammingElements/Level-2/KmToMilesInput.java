2c81092e4f8a086ece451cedab65be6d4680429f
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-19 20:39:19 +0530

    [Prashant Chaudhary] Add. a program to convert km to miles

--- Day1_ProgrammingElements/Level-2/KmToMilesInput.java
+++ Day1_ProgrammingElements/Level-2/KmToMilesInput.java
@@ -1,5 +1,10 @@
 package Day2;
 
-public class KmToMilesInput {
+public class KmToMiles {
     
+    public static void main(String[] args) {
+        double km = 10.8;
+        double miles = km * 1.6;
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
 }
