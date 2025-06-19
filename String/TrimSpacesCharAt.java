44d700cf0b8cf32e9bc8561e49231021f7b47d90
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-19 08:21:20 +0530

    [Prashant chaudhary] Add.  a program to trim the leading and trailing spaces from a string using the charAt() method

--- String/TrimSpacesCharAt.java
+++ String/TrimSpacesCharAt.java
@@ -15,7 +15,6 @@ public class TrimSpacesCharAt {
         }
         return sb.toString();
     }
-
     public static boolean compareByCharAt(String s1, String s2) {
         if (s1.length() != s2.length()) return false;
         for (int i = 0; i < s1.length(); i++) {
