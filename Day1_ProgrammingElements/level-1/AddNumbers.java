413da87ddd1ecc3bc8ea57af00ae18d3eb355067
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-19 20:25:15 +0530

    [Prashant Chaudhary] Add. a program to add two numbers

--- Day1_ProgrammingElements/level-1/AddNumbers.java
+++ Day1_ProgrammingElements/level-1/AddNumbers.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class AddNumbers {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first number: ");
