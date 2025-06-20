f6e5495a6970c7ad2b31373defd0c097de26d8e1
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-20 09:13:55 +0530

    [Prashant Chaudhary] Add. a program to find maximum of three numbers

--- String/Built-inFunctions/MaximumOfThree.java
+++ String/Built-inFunctions/MaximumOfThree.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class MaximumOfThree {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
