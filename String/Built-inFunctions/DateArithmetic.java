ef85015147f37a708e6914296209ef93cb571a7d
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-20 08:48:08 +0530

    [Prashant Chaudhary] Add. Date Arithmetic program Takes a date input and adds 7 days, 1 month, and 2 years to it.  Then subtracts 3 weeks from the result.

--- String/Built-inFunctions/DateArithmetic.java
+++ String/Built-inFunctions/DateArithmetic.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 
 public class DateArithmetic {
+    
     public static void main(String[] args) {
         LocalDate date = LocalDate.of(2022, 6, 19);
         LocalDate result = date.plusDays(7).plusMonths(1).plusYears(2).minusWeeks(3);
