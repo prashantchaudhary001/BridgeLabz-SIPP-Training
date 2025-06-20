ef1f142c2f5776c3edfd2c376790bb7b4cc7dec0
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-20 09:15:40 +0530

    [Prashant Chaudhary] Add. a program to check given number is palindrome or not

--- String/Built-inFunctions/PalindromeChecker.java
+++ String/Built-inFunctions/PalindromeChecker.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class PalindromeChecker {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
