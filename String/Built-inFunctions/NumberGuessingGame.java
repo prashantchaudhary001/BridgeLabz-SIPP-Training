5cf25e9a69cab6d3e6c6d193ed41cc42d4d82f85
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-06-20 09:14:34 +0530

    [Prashant Chaudhary] Add.  a Java program where the user thinks of a number between 1 and 100, and the computer tries to guess the number by generating random guesses.

--- String/Built-inFunctions/NumberGuessingGame.java
+++ String/Built-inFunctions/NumberGuessingGame.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 public class NumberGuessingGame {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
