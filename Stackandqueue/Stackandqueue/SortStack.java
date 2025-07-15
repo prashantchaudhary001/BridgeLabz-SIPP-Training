27f08afb96ee00b54dc4f0fafb063687e94e9607
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-07-15 09:08:42 +0530

    [Prashant Chaudhary] Add. a program to Stock Span Problem Problem: For each day in a stock price array, calculate the span (number of consecutive days the price was less than or equal to the current day's price). Hint: Use a stack to keep track of indices of prices in descending order.

--- Stackandqueue/Stackandqueue/SortStack.java
+++ Stackandqueue/Stackandqueue/SortStack.java
@@ -13,7 +13,6 @@ public class SortStack {
             
             //Sort
             sortStack(stack);       
-            
             //Insert
             insertInSortedOrder(stack, top);
         }
