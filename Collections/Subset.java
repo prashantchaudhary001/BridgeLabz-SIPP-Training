0ef55272ca6c7f352e13147e5717dfad84752b48
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-07-31 23:28:48 +0530

    [Prashant Chaudhary] Add. a program to Find Subsets Check if one set is a subset of another. Example: Set1: {2, 3}, Set2: {1, 2, 3, 4} → Output: true

--- Collections/Subset.java
+++ Collections/Subset.java
@@ -8,7 +8,6 @@ public class Subset {
         //Create two sets
         Set<Integer> set1 = new HashSet<>(Arrays.asList(2, 3));            
         Set<Integer> set2 = new HashSet<>(Arrays.asList(1, 2, 3, 4));      
-
         //Check if set2 contains all elements of set1
         boolean isSubset = set2.containsAll(set1);
 
