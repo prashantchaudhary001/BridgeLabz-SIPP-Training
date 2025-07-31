a04755e66b3bf079b507ad590883ff8aefe501d1
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-07-31 23:30:20 +0530

    [Prashant Chaudhary] Add. a program to Check if Two Sets Are Equal Compare two sets and determine if they contain the same elements, regardless of order. Example: Set1: {1, 2, 3}, Set2: {3, 2, 1} → Output: true.

--- Collections/TwoSetsEqual.java
+++ Collections/TwoSetsEqual.java
@@ -8,7 +8,6 @@ public class TwoSetsEqual {
         //Create two sets
         Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
         Set<Integer> set2 = new HashSet<>(Arrays.asList(3, 2, 1));
-
         //Compare the sets 
         boolean areEqual = set1.equals(set2);
         System.out.println("Are both sets equal? " + areEqual);
