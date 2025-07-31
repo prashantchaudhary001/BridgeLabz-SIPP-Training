f8d50b53ac28455080acafe82dee6bc6f460f2b5
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-07-31 23:29:31 +0530

    [Prashant Chaudhary] Add. a program to Symmetric Difference Find the symmetric difference (elements present in either set but not in both) of two sets. Example: Set1: {1, 2, 3}, Set2: {3, 4, 5} → Output: {1, 2, 4, 5}.

--- Collections/SymmetricDifference.java
+++ Collections/SymmetricDifference.java
@@ -7,7 +7,6 @@ public class SymmetricDifference {
         //Create two sets
         Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
         Set<Integer> set2 = new HashSet<>(Arrays.asList(3, 4, 5));
-
         //Create copies of both sets
         Set<Integer> symDiff = new HashSet<>(set1);
         Set<Integer> temp = new HashSet<>(set2);
