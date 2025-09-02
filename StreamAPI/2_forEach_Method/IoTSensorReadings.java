f4da15142bcb1804d8f6671b587d04f24034881e
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-09-02 09:01:41 +0530

    [Prashant Chaudhary] add programs of Stream API in java

---
+++ StreamAPI/2_forEach_Method/IoTSensorReadings.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Java Stream API Example Placeholder");
+    }
+}
